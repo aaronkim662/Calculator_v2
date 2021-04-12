e05dd79ae3e711b48034172ceb389409074d73a4
aaron <aaronkim662@gmail.com>
2021-04-11 21:39:01 -0400

    added calculator function

--- src/Utils/calculator.js
+++ src/Utils/calculator.js
@@ -155,3 +155,53 @@ const pemdas = (calcArray = [], ) => {
   return calcArray[0];
 };
 
+// do a loop
+// find the ele
+// save the index
+// insert
+
+// be able to do simple operations first
+// check for parenthesis
+// so use operations
+// have functions for + - * /
+
+const calculator = (input = '') => {
+  const parenthesis = ['(', ')'];
+  let invalidInput = checkEdgeCases(input);
+  let newInput = [];
+  let calculation = 0;
+  let startParens = true;
+
+  // check for edge cases first
+  if(!invalidInput){
+    console.log('Syntax Error')
+    return 'Syntax Error'
+  }
+  // replace double negatives with a +
+  let format = input.replace(/--/g, '+');
+  // split elements into an array
+  newInput = splitElements(format);
+  // format the negative numbers
+  newInput = formatNegative(newInput);
+  // format the parenthesis
+  newInput = formatParens(newInput);
+
+  // do calculations within parenthesis first
+  while(startParens){
+    if(newInput.includes(parenthesis[0]) && newInput.includes(parenthesis[1])){
+      let firstBracketIdx = newInput.indexOf('(');
+      let secondBracketIdx = newInput.indexOf(')');
+      let innerValue = newInput.slice(firstBracketIdx + 1, secondBracketIdx);
+      let innerCalc = pemdas(innerValue);
+      newInput.splice(firstBracketIdx, secondBracketIdx + 1, innerCalc);
+    }else{
+      startParens = false;
+    }
+  }
+
+  // do remaining calculations
+  calculation = pemdas(newInput);
+  console.log(calculation);
+  return calculation;
+}
+
