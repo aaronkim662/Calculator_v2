226423ee7341067ad9f4cb98e6b6bf109e89ed86
aaron <aaronkim662@gmail.com>
2021-04-11 23:19:10 -0400

    added cells to app

--- src/App.jsx
+++ src/App.jsx
@@ -4,4 +4,35 @@ import Calculator from './Utils/calculator';
 import CalculatorCell from './App/SharedComponents/CalculatorCell/CalculatorCell';
 
 const App = () => {
-  const [input, setInput] = React.useState()
+  let [input, setInput] = React.useState()
+
+  const onDigitClick = (value) => {
+    setInput(value)
+    console.log(value)
+  }
+
+  const cells = (row) => {
+    const list = row.map((number) =>
+      <CalculatorCell digit={number} key = {'Cell_' +number} onClick={() => onDigitClick(number)} />
+      )
+    return (
+      <div className = 'CalculatorRow' >{list}</div>
+    )
+  }
+
+  return (
+    <div className="App">
+      <div>Calculator</div>
+      <input></input>
+      <div className = 'Calculator'>
+        {cells(['(', ')', '' , 'AC'])}
+        {cells([7,8,9, '/'])}
+        {cells([4,5,6, '*'])}
+        {cells([1,2,3, '-'])}
+        {cells([0,'.','=', '+'])}
+      </div>
+    </div>
+  );
+}
+
+export default App;
