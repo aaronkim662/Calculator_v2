993171eefbb005cd6402566edf840f51efa7b3f2
aaron <aaronkim662@gmail.com>
2021-04-13 10:42:19 -0400

    added setClasses function

--- src/App/SharedComponents/CalculatorCell/CalculatorCell.jsx
+++ src/App/SharedComponents/CalculatorCell/CalculatorCell.jsx
@@ -6,6 +6,24 @@ const CalculatorCell = (props) => {
     digit: props.digit.toString()
   });
 
+  const setClasses = (value) => {
+    const numbers = '0123456789';
+    let classes = 'CalculatorCell'
+    if(numbers.includes(value)){
+      classes += ' CellNumbers'
+    }
+    if(value === '(' || value === ')' || value === " " || value === 'AC' || value === '*' || value === '/' || value === '-' || value === '+'){
+      classes += ' CellUpperLevel'
+    }
+    if(value === '='){
+      classes += ' CellEquals'
+    }
+    if(value === '.'){
+      classes += ' CellDecimal'
+    }
+    return classes
+  }
+
   return (
     <div id = {'Cell_' + digit.digit} className = 'CalculatorCell' onClick = {() => props.onDigitClick(digit.digit)}>
       {digit.digit}
