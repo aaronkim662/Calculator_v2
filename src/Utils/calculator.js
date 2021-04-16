4f4c039d396b03b0b17be1b5741618760aabe36a
aaron <aaronkim662@gmail.com>
2021-04-16 11:23:48 -0400

    edited formatParens function

--- src/Utils/calculator.js
+++ src/Utils/calculator.js
@@ -116,6 +116,11 @@ const formatParens = (calcInput = []) => {
     if(calcInput[i] === parenthesis[0] && calcInput[i+1] === parenthesis[1]){
       calcInput.splice(i+ 1, 0, '*')
       i++
+    // if there is a parenthesis and a number next to it in either order it is multiplication
+    }else if(!isNaN(parseFloat(calcInput[i])) && calcInput[i+1] === parenthesis[1]){
+      calcInput.splice(i+1, 0, '*')
+    }else if(!isNaN(parseFloat(calcInput[i+1])) && calcInput[i] === parenthesis[0]){
+      calcInput.splice(i+1, 0, '*')
     }
   }
   return calcInput
